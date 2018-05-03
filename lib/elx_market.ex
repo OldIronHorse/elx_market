7e7f6810565cf43baf506c456c5795107c43a60f
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-05-03 22:12:20 +0100

    Initial mix-generated project

---
+++ lib/elx_market.ex
@@ -0,0 +1,18 @@
+defmodule ElxMarket do
+  @moduledoc """
+  Documentation for ElxMarket.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElxMarket.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elx_market_test.exs
@@ -0,0 +1,8 @@
+defmodule ElxMarketTest do
+  use ExUnit.Case
+  doctest ElxMarket
+
+  test "greets the world" do
+    assert ElxMarket.hello() == :world
+  end
+end
