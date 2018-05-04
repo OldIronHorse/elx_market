759eb3ae9ffba8646daa2a170d32a991775b53b2
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-05-04 12:45:06 +0100

    simplest 3 for 2 working

--- lib/elx_market.ex
+++ lib/elx_market.ex
@@ -4,18 +4,34 @@ defmodule ElxMarket do
   """
 
   def price_basket(basket, prices) do
-    price_basket(basket, prices, [])
-  end
-
-  def price_basket([], _prices, priced_items) do
-    Enum.reverse(priced_items)
-  end
-
-  def price_basket([item | basket], prices, priced_items) do
-    price_basket(basket, prices, [{item, prices[item]} | priced_items])
+    Enum.map(basket, fn item -> {item, prices[item]} end)
   end
 
   def three_for_two(eligible_item_name, full_price_items, discounted_items) do
-    {full_price_items, discounted_items}
+    {discounts, undiscounted} =
+      make_discounts_three_for_two(
+        Enum.filter(full_price_items, fn {name, _price} -> name == eligible_item_name end),
+        discounted_items
+      )
+
+    {Enum.concat(
+       undiscounted,
+       Enum.filter(full_price_items, fn {name, _price} -> name != eligible_item_name end)
+     ), discounts}
+  end
+
+  def make_discounts_three_for_two(eligible_items, discounted_items)
+      when length(eligible_items) < 3 do
+    {discounted_items, eligible_items}
+  end
+
+  def make_discounts_three_for_two(eligible_items, discounted_items) do
+    {[make_three_for_two(Enum.take(eligible_items, 3)) | discounted_items],
+     Enum.drop(eligible_items, 3)}
+  end
+
+  def make_three_for_two([free | paid]) do
+    {_name, saving} = free
+    {[free | paid], saving, Enum.reduce(paid, 0, fn {_name, price}, total -> total + price end)}
   end
 end
--- test/elx_market_test.exs
+++ test/elx_market_test.exs
@@ -1,6 +1,9 @@
 defmodule ElxMarketTest do
   use ExUnit.Case
+  import Enum
   doctest ElxMarket
+
+  # TODO: use records or dictionaries for items?
 
   test "price a basket" do
     prices = %{"soap" => 1.5, "shampoo" => 2.0, "toothpaste" => 0.8, "bannana" => 0.8}
@@ -18,21 +21,44 @@ defmodule ElxMarketTest do
   test "3 for 2: not triggered, no triplet" do
     assert ElxMarket.three_for_two(
              "shampoo",
+             sort([
+               {"soap", 1.5},
+               {"shampoo", 2.0},
+               {"shampoo", 2.0},
+               {"toothpaste", 0.8},
+               {"soap", 1.5}
+             ]),
+             []
+           ) ==
+             {sort([
+                {"soap", 1.5},
+                {"shampoo", 2.0},
+                {"shampoo", 2.0},
+                {"toothpaste", 0.8},
+                {"soap", 1.5}
+              ]), []}
+  end
+
+  test "3 for 2: simeplest" do
+    assert ElxMarket.three_for_two(
+             "shampoo",
              [
                {"soap", 1.5},
                {"shampoo", 2.0},
                {"shampoo", 2.0},
                {"toothpaste", 0.8},
+               {"shampoo", 2.0},
                {"soap", 1.5}
              ],
              []
            ) ==
              {[
                 {"soap", 1.5},
-                {"shampoo", 2.0},
-                {"shampoo", 2.0},
                 {"toothpaste", 0.8},
                 {"soap", 1.5}
-              ], []}
+              ],
+              [
+                {[{"shampoo", 2.0}, {"shampoo", 2.0}, {"shampoo", 2.0}], 2.0, 4.0}
+              ]}
   end
 end
